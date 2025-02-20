1a16f6f711e459b297f3983104c62ba5f4f0715f
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-20 23:19:42 +0200

    Add files via upload

---
+++ prac24.2/Avto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24._2
+{
+    internal class Avto
+    {
+        private string model;
+        private string color;
+        private int speed;
+
+        public string Model
+        {
+            get
+            {
+                if (model == null || model == "") return "error";
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                if (color == null || color == "") return "error";
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value < 60) speed = 60;
+                else if (value > 150) speed = 150;
+                else speed = value;
+            }
+        }
+        public Avto()
+        {
+
+        }
+
+        public Avto(string model, string color, int speed)
+        { 
+            this.Model = model;
+            this.Color = color;
+            this.Speed = speed;
+        }
+        public string Output()
+        {
+            return $"model: {Model} \ncolor: {Color} \nspeed: {Speed}";
+        }
+
+    }
+}
---
+++ prac24.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto1 = new Avto();
+            Console.WriteLine(avto1.Output());
+            Console.WriteLine();
+
+            Avto avto2 = new Avto("BMV","red",140);
+            Console.WriteLine(avto2.Output());
+            Console.WriteLine();
+
+            Avto avto3 = new Avto("mers", "" , 50);
+            Console.WriteLine(avto3.Output());
+            Console.WriteLine();
+            Console.Write("model: ");
+            string model1 = Console.ReadLine();
+            Console.Write("color: ");
+            string color1 = Console.ReadLine();
+            Console.Write("speed: ");
+            int speed1 = int.Parse(Console.ReadLine());
+            Avto avto4 = new Avto(model1,color1,speed1);
+            Console.WriteLine(avto4.Output());
+        }
+    }
+}
