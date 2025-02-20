cf28e12e6115ce8f0e43c724d2055475a8bd8ef9
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-20 10:46:49 +0200

    Add files via upload

---
+++ prac24/Avto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24
+{
+    class Avto
+    { 
+        //поля
+        private string brand;
+        private string color;
+        private double speed;
+        //конструктор без параметров
+
+        public Avto()
+        {
+
+        } 
+        //конструктор с 3 параметрами 
+        public Avto(string brand1, string color1, double speed1)
+        {
+            this.Brand = brand1;
+            this.Color = color1;
+            this.Speed = speed1;
+        }
+        //свойства без валидации
+        public string Brand { get { return brand; } set { brand = value; } }
+        public string Color { get { return color; } set {color = value; } }
+
+        //свойство speed с валидацией для проверки данных
+        public double Speed 
+        { 
+            get { return speed; } 
+            set { 
+                if (value < 20) speed = 20; 
+                else if (value > 120) speed = 120;
+                else speed = value; }  }
+
+        //метод вывода
+        public string OutPut()
+        {
+            return $"\n brand = {brand} \n color = {color} \n speed = {speed}";
+        }
+    }
+}
---
+++ prac24/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto1 = new Avto();
+
+            avto1.Brand = "toyota";
+            avto1.Color = "red";
+            avto1.Speed = 15;
+            Console.WriteLine(avto1.OutPut());
+
+            Avto avto2 = new Avto("BMV", "black", 150);
+            Console.WriteLine(avto2.OutPut());
+
+            Console.Write("Введите марку машины: ");
+            string brand = Console.ReadLine();
+            Console.Write("Введите цвет машины: ");
+            string color = Console.ReadLine();
+            Console.Write("Введите скорость машины: ");
+            int speed = int.Parse(Console.ReadLine());
+            Avto avto3 = new Avto(brand, color,speed);
+            Console.WriteLine(avto3.OutPut());
+
+            Console.Read();
+        }
+    }
+}
