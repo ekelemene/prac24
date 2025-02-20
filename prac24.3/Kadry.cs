86600517212c33739cfeee9079cd499ea7adff30
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-21 00:13:40 +0200

    Add files via upload

---
+++ prac24.3/Kadry.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24._3
+{
+    internal class Kadry
+    {
+        private string surname;
+        private int age;
+        private string dol;
+        private int staj;
+
+        public Kadry()
+        {
+
+        }
+
+        public Kadry(string surname, int age, string dol, int staj)
+        {
+            this.Surname = surname;
+            this.Age = age;
+            this.Dol = dol;
+            this.Staj = staj;
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value == null || value == "" ? "error" : value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 16) age = 16;
+                else if (value > 60) age = 60;  
+                else age = value;
+            }
+        }
+        public string Dol
+        {
+            get
+            {
+                return dol;
+            }
+            set
+            {
+                dol = value == null || value == "" ? "error" : value;
+            }
+        }
+        public int Staj
+        {
+            get
+            {
+                return staj;
+            }
+            set
+            {
+                if (value < 0) staj = 0;
+                else if (value > 45) staj = 45;
+                else staj = value;
+            }
+        }
+
+         public string Output()
+        {
+            return $"surname: {Surname} \nage: {Age} \ndol: {Dol} \nstaj: {Staj}";
+        }
+
+
+    }
+}
---
+++ prac24.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kadry kadr1 = new Kadry();
+            kadr1.Surname = "Belouschenko";
+            kadr1.Age = 17;
+            kadr1.Dol = "student";
+            kadr1.Staj = 10;
+            Console.WriteLine(kadr1.Output());
+            Console.WriteLine();
+
+            Kadry kadr2 = new Kadry("Pushkin", 60, "", 60);
+            Console.WriteLine(kadr2.Output());
+            Console.WriteLine();
+            Console.Write("surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("dol: ");
+            string dol = Console.ReadLine();
+            Console.Write("staj: ");
+            int staj = int.Parse(Console.ReadLine());
+            Kadry kadr3 = new Kadry(surname, age, dol, staj);
+            Console.WriteLine(kadr3.Output());
+        }
+    }
+}
